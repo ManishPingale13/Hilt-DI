897e47183b422e9be925eb5254be0aa3bdefcff5
Manish Pingale <manishpingale2005@gmail.com>
2020-12-19 16:07:34 +0530

    Field Injection VS Constructor Injection

--- app/src/main/java/com/example/hilt/MainActivity.kt
+++ app/src/main/java/com/example/hilt/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.hilt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    //Field Injection
+    @Inject
+    lateinit var someClass: someClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(someClass.doaThing())
+        println(someClass.doSomeOtherThing())
+    }
+}
+
+class someClass
+@Inject constructor(private val someOtherClass: someOtherClass) {
+
+    fun doaThing(): String {
+        return "Did A Thing!"
+    }
+
+    fun doSomeOtherThing(): String {
+        return someOtherClass.doSomeOtherThing()
+    }
+}
+
+class someOtherClass @Inject constructor() {
+    fun doSomeOtherThing(): String {
+        return "Did Some Other Thing!"
     }
 }
